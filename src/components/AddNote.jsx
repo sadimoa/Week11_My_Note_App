a290ec497c92a3d55ef78824845a0504eb2dfa82
sadia mahamoud <sadimoa06@gmail.com>
2024-03-11 23:43:36 +0300

    working updating note

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,34 +1,33 @@
 import React from "react";
-
-// Import Formik and Yup
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, useFormikContext } from "formik";
 import * as Yup from "yup";
-const AddNote = (props) => {
-  // add form logic here
 
+const AddNote = (props) => {
   const initialValues = {
     title: "",
     content: "",
   };
 
   const validationScheme = Yup.object({
-    title: Yup.string().required("title is required"),
-    content: Yup.string().required("required"),
+    title: Yup.string().required("Title is required"),
+    content: Yup.string().required("Content is required"),
   });
 
-  const handleSubmit = (values) => {
+  const handleSubmit = (values, { resetForm }) => {
     props.createNote({
       title: values.title,
       content: values.content,
     });
+    resetForm();
   };
 
   return (
     <div className="w-[520px] p-12 py-8 mb-10 rounded bg-white h-auto ">
-      <Formik 
-      initialValues={initialValues} 
-      validationSchema={validationScheme}
-      onSubmit={handleSubmit}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationScheme}
+        onSubmit={handleSubmit}
+      >
         <Form className="flex flex-col space-y-5">
           <Field
             className="border p-1.5 rounded"
@@ -36,15 +35,15 @@ const AddNote = (props) => {
             name="title"
             placeholder="Title"
           />
-          <ErrorMessage component="div" name='title' className='text-red-300'/>
+          <ErrorMessage component="div" name="title" className="text-red-300" />
 
           <Field
             className="border p-1.5 rounded"
             type="text"
             name="content"
-            placeholder="content"
+            placeholder="Content"
           />
-          <ErrorMessage component="div" name='content' className='text-red-300'/>
+          <ErrorMessage component="div" name="content" className="text-red-300" />
           <button className="px-12 py-2 rounded bg-yellow-500" type="submit">
             Add Note
           </button>
